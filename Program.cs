bc6708ef87af134bd1d96bc5b5177db0d130926a
Lynx2209 <csi23-jconazo@colegioaltair.net>
2024-04-01 13:21:44 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ejercicio3rep
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Linea:");
+            int linea = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Texto:");
+            string texto = Console.ReadLine();
+
+            StreamWriter sw;
+            sw = File.CreateText("archivo.txt");
+
+
+
+        }
+    }
+}
